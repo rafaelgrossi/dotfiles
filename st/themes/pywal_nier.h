0a81f0e6657c054027cd72f0f3e33f3d96d9a546
rafaelgrossi <rafaelgrossi@users.noreply.github.com>
2019-05-14 15:06:31 -0300

    new theme

---
+++ st/themes/pywal_nier.h
@@ -0,0 +1,43 @@
+/*********************************************************************************************
+**  pywal_nier.h
+**  themes												
+**																							
+**  Created by Rafael Grossi on 05/14/19.				
+**  Copyright 2019. Rafael Grossi. All rights reserved.	
+*********************************************************************************************/
+
+#ifndef pywal_nier_h
+#define pywal_nier_h
+
+const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#2c2a25", /* black   */
+  [1] = "#cd664d", /* red     */
+  [2] = "#999583", /* green   */
+  [3] = "#A29D8A", /* yellow  */
+  [4] = "#727B5B", /* blue    */
+  [5] = "#4E4B42", /* magenta */
+  [6] = "#3a372e", /* cyan    */
+  [7] = "#d8d5ca", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#97958d",  /* black   */
+  [9]  = "#cd664d",  /* red     */
+  [10] = "#999583", /* green   */
+  [11] = "#A29D8A", /* yellow  */
+  [12] = "#727B5B", /* blue    */
+  [13] = "#4E4B42", /* magenta */
+  [14] = "#3a372e", /* cyan    */
+  [15] = "#d8d5ca", /* white   */
+
+  /* special colors */
+  [256] = "#cd664d",     /* cursor */
+  [257] = "#cd664d",     /* cursor */
+  [258] = "#dad3ba",  //"#dcd6bd", /* background */
+  [259] = "#2c2a25", /* foreground */
+};
+
+
+#endif /* pywal_nier_h */
+
