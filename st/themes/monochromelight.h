ff59517631e64d25decdf08fc8f1b997c8f38a98
rafaelgrossi <rafaelgrossi@users.noreply.github.com>
2019-05-15 12:21:15 -0300

    new st theme

---
+++ st/themes/monochromelight.h
@@ -0,0 +1,42 @@
+/*********************************************************************************************
+ **  monochrome.h
+ **  themes												
+ **																							
+ **  Created by Rafael Grossi on 05/13/19.				
+ **  Copyright 2019. Rafael Grossi. All rights reserved.	
+ *********************************************************************************************/
+
+#ifndef monochrome_h
+#define monochrome_h
+
+static const char *colorname[] = {
+
+	/* 8 normal colors */
+	[0] = "#3c3828", /* black   */
+	[1] = "#3c3828", /* red     */
+	[2] = "#3c3828", /* green   */
+	[3] = "#3c3828", /* yellow  */
+	[4] = "#3c3828", /* blue    */
+	[5] = "#3c3828", /* magenta */
+	[6] = "#3c3828", /* cyan    */
+	[7] = "#3c3828", /* white   */
+
+	/* 8 bright colors */
+	[8]  = "#3c3828", /* black   */
+	[9]  = "#3c3828", /* red     */
+	[10] = "#3c3828", /* green   */
+	[11] = "#3c3828", /* yellow  */
+	[12] = "#3c3828", /* blue    */
+	[13] = "#3c3828", /* magenta */
+	[14] = "#3c3828", /* cyan    */
+	[15] = "#3c3828", /* white   */
+
+	/* special colors */
+  	[256] = "#cd664d",     /* cursor */
+  	[257] = "#cd664d",     /* cursor */
+  	[258] = "#d4ceb5", /* foreground */
+ 	[259] = "#3c3828", /* background */
+};
+
+#endif /* monochrome_h */
+
